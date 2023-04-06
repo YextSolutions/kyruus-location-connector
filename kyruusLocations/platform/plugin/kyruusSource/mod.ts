b046f8823959efd1bcc1a7bc46678a52caf65915
hlrich <99983356+hlrich@users.noreply.github.com>
2023-04-06 12:42:13 -0400

    Add files via upload

---
+++ kyruusLocations/platform/plugin/kyruusSource/mod.ts
@@ -0,0 +1,114 @@
+declare const clientId: string;
+declare const clientSecret: string;
+declare const customer: string;
+
+const requestOptions = {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: new URLSearchParams ({
+        'grant_type': 'client_credentials',
+        'client_id': `${clientId}`,
+        'client_secret': `${clientSecret}`
+    })
+}
+
+//fetch URLs for Kyruus
+let providerUrl = `https://api.kyruus.com/v9/${customer}/providers?per_page=50&sort=name&availability_fields=-ALL&exclude_from_analytics=true`;
+let locationUrl = `https://api.kyruus.com/v9/${customer}/locations?per_page=50&sort=name&exclude_from_analytics=true`;
+
+//fetchProviders
+export const fetchProviders = async (inputString) => {
+    //fetch access token
+    var tokenRequest = await fetch ('https://api.kyruus.com/oauth2/token', requestOptions).then(response => response.json());
+    var accessToken = tokenRequest.access_token;
+
+    var requestOptionsNew = {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${accessToken}`
+        }
+    }
+    
+    //fetch providers info
+    const inputJson = JSON.parse(inputString);
+    const pageToken = inputJson.pageToken;
+  
+    let urlParams = new URLSearchParams(providerUrl.slice(providerUrl.indexOf('?')));
+    let currentPage = urlParams.get('page');
+  
+    // Reset the providerUrl variable to its original value before appending the pageToken parameter
+    let updatedProviderUrl = providerUrl.split('&page=')[0];
+  
+    if (pageToken && currentPage !== pageToken) {
+      // Append the pageToken parameter to the updatedProviderUrl
+      updatedProviderUrl = `${updatedProviderUrl}&page=${pageToken}`;
+    }
+  
+    const providers = await fetch(updatedProviderUrl, requestOptionsNew).then((response) =>
+      response.json()
+    );
+  
+    if (providers._metadata.provider_count === 0) {
+      const response = { data: [] };
+      const stringify = JSON.stringify(response);
+      return stringify;
+    } else {
+      const response = { data: providers };
+      if (pageToken) {
+        response['nextPageToken'] = parseInt(pageToken) + 1;
+      } else {
+        response['nextPageToken'] = 2;
+      }
+      const stringify = JSON.stringify(response);
+      return stringify;
+    }
+  };
+
+  //fetchLocations
+
+  export const fetchLocations = async (inputString) => {
+    //fetch access token
+    var tokenRequest = await fetch ('https://api.kyruus.com/oauth2/token', requestOptions).then(response => response.json());
+    var accessToken = tokenRequest.access_token;
+
+    var requestOptionsNew = {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${accessToken}`
+        }
+    }
+    //fetch locations info
+    const inputJson = JSON.parse(inputString);
+    const pageToken = inputJson.pageToken;
+  
+    let urlParams = new URLSearchParams(locationUrl.slice(locationUrl.indexOf('?')));
+    let currentPage = urlParams.get('page');
+  
+    // Reset the LocationUrl variable to its original value before appending the pageToken parameter
+    let updatedLocationUrl = locationUrl.split('&page=')[0];
+  
+    if (pageToken && currentPage !== pageToken) {
+      // Append the pageToken parameter to the updatedLocationUrl
+      updatedLocationUrl = `${updatedLocationUrl}&page=${pageToken}`;
+    }
+    const locations = await fetch(updatedLocationUrl, requestOptionsNew).then((response) =>
+      response.json()
+    );
+    if (locations._locations.length === 0) {
+      const response = { data: [] };
+      const stringify = JSON.stringify(response);
+      return stringify;
+    } else {
+      const response = { data: locations };
+      if ((locations._locations).length > 0 && pageToken) {
+      // if (pageToken) {
+        response['nextPageToken'] = parseInt(pageToken) + 1;
+      } else {
+        response['nextPageToken'] = 2;
+      }
+      const stringify = JSON.stringify(response);
+      return stringify;
+    }
+  };
